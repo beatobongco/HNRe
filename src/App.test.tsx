b4581e88a5fc330feaf344e38b3b9ded79acd1e0
Beato Bongco <beatobongco@gmail.com>
2021-02-18 21:16:33 +0800

    Add initial tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
+import StoryCard from "./components/StoryCard";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { debug } from "console";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const server = setupServer(
+  rest.get("/newstories.json", (req, res, ctx) => {
+    return res(ctx.json(Array.from({ length: 500 }, (_, i) => i)));
+  }),
+  rest.get("/item/:itemId", (req, res, ctx) => {
+    const { itemId } = req.params;
+    const parsedId = parseInt(itemId.split(".")[0]);
+    return res(
+      ctx.json({
+        by: `Testy McTestFace ${parsedId}`,
+        descendants: 0,
+        id: parsedId,
+        score: 100,
+        time: 1613642412,
+        title:
+          "Doist is leveraging natural language processing to skyrocket productivity and mental wellbeing",
+        type: "story",
+        url: "https://blog.doist.com/remote-work-mental-health/",
+      })
+    );
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test("renders App", async () => {
+  render(<App apiURL="" />);
+  expect(screen.getByRole("heading")).toHaveTextContent("HNRe");
+  // TODO: mock IntersectionObserver properly, so I can test rendering of articles
+});
+
+test("renders StoryCard", async () => {
+  const id = 1337;
+  render(
+    <StoryCard
+      apiURL=""
+      key={id}
+      storyId={id}
+      isOnline={true}
+      isLastItem={false}
+      updateMaxStory={() => {}}
+    />
+  );
+  // TODO: refactor fetching to be in an api module, so can separate testing of getting data
+  // and rendering components
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,16 @@
+// @ts-nocheck
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+const observe = jest.fn();
+const unobserve = jest.fn();
+const io = jest.fn(() => ({
+  observe,
+  unobserve,
+}));
+
+window.IntersectionObserver = io;
+global.IntersectionObserver = io;
